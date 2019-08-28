6d1784814e685a98027b0d79c854bbb9e91d6ae5
nmnd <reddigitalsw@gmail.com>
2019-08-28 16:49:35 +0300

    first commit

---
+++ src/states/Game.ts
@@ -0,0 +1,39 @@
+declare var __DEV__: string;
+
+import * as Phaser from 'phaser-ce'
+import Mushroom from '../sprites/Mushroom'
+
+export default class extends Phaser.State {
+  mushroom: Mushroom;
+
+  init () {}
+  preload () {}
+
+  create () {
+    const bannerText = 'Phaser + ES6 + Webpack'
+    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
+    banner.font = 'Bangers'
+    banner.padding.set(10, 16)
+    banner.fontSize = 40
+    banner.fill = '#77BFA3'
+    banner.smoothed = false
+    banner.anchor.setTo(0.5)
+
+    this.mushroom = new Mushroom({
+      // Note: Phaser.State classes called from within Game class
+      // so this is bound to a Phaser.Game
+      game: this,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'mushroom'
+    })
+
+    this.game.add.existing(this.mushroom)
+  }
+
+  render () {
+    if (__DEV__) {
+      this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    }
+  }
+}
